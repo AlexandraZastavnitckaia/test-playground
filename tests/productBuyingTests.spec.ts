36fbc2c805acdebec9c1a5c19c1f1ed6f82f9b1b
AlexandraZastavnitckaia <alexandra.sharonova@gmail.com>
2024-03-01 21:02:39 +0100

    add product to basket test

--- tests/productBuyingTests.spec.ts
+++ tests/productBuyingTests.spec.ts
@@ -21,3 +21,29 @@ test("Navigate to the shopping cart", async ({ page }) => {
   const pageTitle = await page.title();
   expect(pageTitle).toContain("Winkelwagentje");
 });
+
+test("Add product to the basket", async ({ page }) => {
+  const pm = new pageManager(page);
+  await pm.onHomePage().openHomePageWithCookiesAccepted();
+  await pm.onHomePage().searchProductViaSearchbar("playmobil");
+
+  const firstProductOnSearchResults = await page
+    .locator(".product-list>li")
+    .first();
+
+  const productName = await firstProductOnSearchResults
+    .locator('[role="heading"]')
+    .innerText();
+
+  const addToBasketButton = await firstProductOnSearchResults.locator(
+    '[aria-label="In winkelwagen"]'
+  );
+
+  await addToBasketButton.click();
+
+  await expect(await page.title()).toContain("Winkelwagentje");
+
+  await expect(await page.locator(".product-details__title")).toContainText(
+    productName
+  );
+});
